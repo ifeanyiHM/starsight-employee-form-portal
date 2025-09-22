ca04ba5899d9ebce3abe1fd9a81d8ba3cba75dd8
ifeanyihm <iheme.ifeanyi@yahoo.com>
2025-09-22 16:30:28 +0100

    email added as receiver

--- app/api/send-pdf/route.ts
+++ app/api/send-pdf/route.ts
@@ -52,7 +52,11 @@ export async function POST(req: NextRequest) {
 
     const info = await transporter.sendMail({
       from: process.env.SMTP_USER,
-      to: ["humanresorces@starsightenergy.com", "obianorue123@gmail.com"],
+      to: [
+        "namadike@starsightenergy.com",
+        "humanresources@starsightenergy.com",
+        "obianorue123@gmail.com",
+      ],
       // to: "humanresorces@starsightenergy.com",
       subject: "Form Submission Confirmation",
       html: `
