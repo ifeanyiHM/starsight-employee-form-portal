c9f81aa3a61dbe141ce7b105da3aac88a447b170
ifeanyihm <iheme.ifeanyi@yahoo.com>
2026-03-02 21:03:57 +0100

    update

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const { createServer } = require("http");
 const { parse } = require("url");
 const next = require("next");
 
+const port = process.env.PORT || 3000;
 const dev = process.env.NODE_ENV !== "production";
+
 const app = next({ dev });
 const handle = app.getRequestHandler();
 
@@ -10,8 +12,7 @@ app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true);
     handle(req, res, parsedUrl);
-  }).listen(process.env.PORT || 3000, (err) => {
-    if (err) throw err;
-    console.log("Ready on http://localhost:3000");
+  }).listen(port, () => {
+    console.log(`> Ready on port ${port}`);
   });
 });
